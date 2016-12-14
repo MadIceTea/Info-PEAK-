8656ee8a1b49e7b43b139b732dd17b73ea4806f9
Mr. Alex Taniguchi (Wiegman) <awiegman2011@gmail.com>
2016-12-14 23:54:56 +0900

    More error fixing
    Error fixing forever

--- Week 9/intcount_loop.rb
+++ Week 9/intcount_loop.rb
@@ -1,18 +1,21 @@
-def intcount_loop(a,b,c)
-	x = a[i]
-	j = 0
-	for i in 0..(a.length()-1)
-		for j in 0…(b.length()-1)
-			if b[j] == 0
-				b[j] = x
-			else
-				b[j] = b[j] +1
-			end
-			if b[j] != 0 || b[j] != x
-				c[j] = 1
-			else
-			c[j] = c[j] + 1
-			end
+a = [10,9,8,8,6,6,6,3,3,2,2,1]
+b = [0,0,0,0,0,0,0]
+c = [0,0,0,0,0,0,0]
+
+def intcount_sort(a,b,c)
+j = -1
+for i in 0..(a.length() - 1)
+x = a[i]
+if x == a[i-1]
+c[j] = c[j] + 1
+		else
+			j = j + 1
+			b[j] = x
+			c[j] = 1
+		end
 		end
 	end
-end
+	
+intcount_sort(a,b,c)
+print b
+print c
